3f8f07c497dd49f1e43963893910d3c7ad7d9249
강현부(SK컴즈) <kanghb@sk.com>
2025-03-13 10:46:10 +0900

    ComboBox, ListBox 사용법 프로젝트

---
+++ WpfSample/BasicControlSample/MainViewModel.cs
@@ -0,0 +1,101 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using WpfTestApp;
+
+namespace BasicControlSample
+{
+    class MainViewModel : ObservableObject
+    {
+        private IList<Person> _persons = new List<Person>
+        {
+                new Person{ Name = "person0104", Sex = true, Age = 11, Address = "Seoul1" },
+                new Person{ Name = "person0105", Sex = false, Age = 12, Address = "Seoul2" },
+                new Person{ Name = "person0106", Sex = true, Age = 13, Address = "Seoul3" },
+                new Person{ Name = "person0107", Sex = false, Age = 14, Address = "Seoul4" },
+                new Person{ Name = "person0108", Sex = true, Age = 15, Address = "Seoul5" },
+                new Person{ Name = "person0109", Sex = false, Age = 16, Address = "Seoul6" },
+                new Person{ Name = "person0110", Sex = true, Age = 17, Address = "Seoul7" },
+                new Person{ Name = "person0111", Sex = false, Age = 18, Address = "Seoul8" },
+                new Person{ Name = "person0112", Sex = true, Age = 19, Address = "Seoul9" },
+                new Person{ Name = "person0113", Sex = false, Age = 20, Address = "Seoul10" },
+                new Person{ Name = "person0114", Sex = true, Age = 21, Address = "Seoul11" },
+                new Person{ Name = "person0115", Sex = false, Age = 22, Address = "Seoul12" },
+                new Person{ Name = "person0116", Sex = true, Age = 23, Address = "Seoul13" },
+                new Person{ Name = "person0117", Sex = false, Age = 24, Address = "Seoul14" },
+                new Person{ Name = "person0118", Sex = true, Age = 25, Address = "Seoul15" },
+                new Person{ Name = "person0119", Sex = false, Age = 26, Address = "Seoul16" },
+                new Person{ Name = "person0120", Sex = true, Age = 27, Address = "Seoul17" },
+                new Person{ Name = "person0121", Sex = false, Age = 28, Address = "Seoul18" },
+                new Person{ Name = "person0122", Sex = true, Age = 29, Address = "Seoul19" },
+                new Person{ Name = "person0123", Sex = false, Age = 30, Address = "Seoul20" },
+                new Person{ Name = "person0124", Sex = true, Age = 31, Address = "Seoul21" },
+                new Person{ Name = "person0125", Sex = false, Age = 32, Address = "Seoul22" },
+                new Person{ Name = "person0126", Sex = true, Age = 33, Address = "Seoul23" },
+                new Person{ Name = "person0127", Sex = false, Age = 34, Address = "Seoul24" },
+                new Person{ Name = "person0128", Sex = true, Age = 35, Address = "Seoul25" },
+                new Person{ Name = "person0129", Sex = false, Age = 36, Address = "Seoul26" },
+                new Person{ Name = "person0130", Sex = true, Age = 37, Address = "Seoul27" },
+                new Person{ Name = "person0131", Sex = false, Age = 38, Address = "Seoul28" },
+                new Person{ Name = "person0132", Sex = true, Age = 39, Address = "Seoul29" },
+                new Person{ Name = "person0133", Sex = false, Age = 40, Address = "Seoul30" },
+                new Person{ Name = "person0134", Sex = true, Age = 41, Address = "Seoul31" },
+                new Person{ Name = "person0135", Sex = false, Age = 42, Address = "Seoul32" },
+                new Person{ Name = "person0136", Sex = true, Age = 43, Address = "Seoul33" },
+                new Person{ Name = "person0137", Sex = false, Age = 44, Address = "Seoul34" },
+                new Person{ Name = "person0138", Sex = true, Age = 45, Address = "Seoul35" },
+                new Person{ Name = "person0139", Sex = false, Age = 46, Address = "Seoul36" },
+                new Person{ Name = "person0140", Sex = true, Age = 47, Address = "Seoul37" },
+                new Person{ Name = "person0141", Sex = false, Age = 48, Address = "Seoul38" },
+                new Person{ Name = "person0142", Sex = true, Age = 49, Address = "Seoul39" },
+                new Person{ Name = "person0143", Sex = false, Age = 50, Address = "Seoul40" },
+                new Person{ Name = "person0104", Sex = true, Age = 111, Address = "Seoul11" },
+                new Person{ Name = "person0105", Sex = false, Age = 112, Address = "Seoul12" },
+                new Person{ Name = "person0106", Sex = true, Age = 113, Address = "Seoul13" },
+                new Person{ Name = "person0107", Sex = false, Age = 114, Address = "Seoul14" },
+                new Person{ Name = "person0108", Sex = true, Age = 115, Address = "Seoul15" },
+                new Person{ Name = "person0109", Sex = false, Age = 116, Address = "Seoul16" },
+                new Person{ Name = "person0110", Sex = true, Age = 117, Address = "Seoul17" },
+                new Person{ Name = "person0111", Sex = false, Age = 118, Address = "Seoul18" },
+                new Person{ Name = "person0112", Sex = true, Age = 119, Address = "Seoul19" },
+                new Person{ Name = "person0113", Sex = false, Age = 120, Address = "Seoul110" },
+                new Person{ Name = "person0114", Sex = true, Age = 121, Address = "Seoul111" },
+                new Person{ Name = "person0115", Sex = false, Age = 122, Address = "Seoul112" },
+                new Person{ Name = "person0116", Sex = true, Age = 123, Address = "Seoul113" },
+                new Person{ Name = "person0117", Sex = false, Age = 124, Address = "Seoul114" },
+                new Person{ Name = "person0118", Sex = true, Age = 125, Address = "Seoul115" },
+                new Person{ Name = "person0119", Sex = false, Age = 126, Address = "Seoul116" },
+                new Person{ Name = "person0120", Sex = true, Age = 127, Address = "Seoul117" },
+                new Person{ Name = "person0121", Sex = false, Age = 128, Address = "Seoul118" },
+                new Person{ Name = "person0122", Sex = true, Age = 129, Address = "Seoul119" },
+                new Person{ Name = "person0123", Sex = false, Age = 130, Address = "Seoul120" },
+                new Person{ Name = "person0124", Sex = true, Age = 131, Address = "Seoul121" },
+                new Person{ Name = "person0125", Sex = false, Age = 132, Address = "Seoul122" },
+                new Person{ Name = "person0126", Sex = true, Age = 133, Address = "Seoul123" },
+                new Person{ Name = "person0127", Sex = false, Age = 134, Address = "Seoul124" },
+                new Person{ Name = "person0128", Sex = true, Age = 135, Address = "Seoul125" },
+                new Person{ Name = "person0129", Sex = false, Age = 136, Address = "Seoul126" },
+                new Person{ Name = "person0130", Sex = true, Age = 137, Address = "Seoul127" },
+                new Person{ Name = "person0131", Sex = false, Age = 138, Address = "Seoul128" },
+                new Person{ Name = "person0132", Sex = true, Age = 139, Address = "Seoul129" },
+                new Person{ Name = "person0133", Sex = false, Age = 140, Address = "Seoul130" },
+                new Person{ Name = "person0134", Sex = true, Age = 141, Address = "Seoul131" },
+                new Person{ Name = "person0135", Sex = false, Age = 142, Address = "Seoul132" },
+                new Person{ Name = "person0136", Sex = true, Age = 143, Address = "Seoul133" },
+                new Person{ Name = "person0137", Sex = false, Age = 144, Address = "Seoul134" },
+                new Person{ Name = "person0138", Sex = true, Age = 145, Address = "Seoul135" },
+                new Person{ Name = "person0139", Sex = false, Age = 146, Address = "Seoul136" },
+                new Person{ Name = "person0140", Sex = true, Age = 147, Address = "Seoul137" },
+                new Person{ Name = "person0141", Sex = false, Age = 148, Address = "Seoul138" },
+                new Person{ Name = "person0142", Sex = true, Age = 149, Address = "Seoul139" },
+                new Person{ Name = "person0143", Sex = false, Age = 150, Address = "Seoul140" },
+        };
+
+        public IList<Person> Persons { get { return _persons; } }
+        public MainViewModel()
+        {
+            Init();
+        }
+
+        private void Init() { }
+
+    }
+}
