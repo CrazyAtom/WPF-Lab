c17ecdce1474b2ca128e901ddf3028df75fb5275
강현부(SK컴즈) <kanghb@sk.com>
2025-03-14 15:20:52 +0900

    TabControl에 Region 설정 - TabItem Header를 통해 빠른 탐색 가능

---
+++ PrismStep1/PrismStep5/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System.Windows.Input;
+
+namespace PrismStep5.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ICommand NavigationCommand { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            NavigationCommand = new DelegateCommand<string>(OnNavigation);
+        }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+            : this()
+        {
+            //IRegionManager를 Injection 받아서 사용하기 위해 _regionManager에 연결합니다.
+            _regionManager = regionManager;
+            //ContentRegion에 Test1View를 등록 예약 시켜 놓습니다.
+            _regionManager.RegisterViewWithRegion("ContentRegion", "Test1View");
+        }
+        private void OnNavigation(string obj)
+        {
+            switch (obj)
+            {
+                case "Back":
+                    {
+                        IRegionNavigationJournal journal = _regionManager.Regions["ContentRegion"].NavigationService.Journal;
+                        if (journal.CanGoBack)
+                        {
+                            journal.GoBack();
+                        }
+                    }
+                    break;
+                default:
+                    _regionManager.RequestNavigate("ContentRegion", obj);
+                    break;
+            }
+        }
+    }
+}
---
+++ PrismStep1/PrismStep5/ViewModels/Test1ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Prism;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PrismStep5.ViewModels
+{
+    public class Test1ViewModel : BindableBase, INavigationAware, IActiveAware
+    {
+        public string Header { get; set; }
+        private IList<string> _messages = new ObservableCollection<string>();
+
+        public event EventHandler IsActiveChanged;
+        private bool _isActive;
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set
+            {
+                SetProperty(ref _isActive, value,
+                    () => Messages.Add($"{GetType().Name} IsActive : {IsActive}"));
+            }
+        }
+
+        public IList<string> Messages
+        {
+            get { return _messages; }
+            set { SetProperty(ref _messages, value); }
+        }
+
+        public Test1ViewModel()
+        {
+            Header = GetType().Name;
+            Messages.Add(Header);
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            Messages.Add($"{GetType().Name} OnNavigatedFrom");
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            Messages.Add($"{GetType().Name} OnNavigatedTo - {navigationContext.NavigationService.Journal.CurrentEntry}");
+        }
+    }
+}
